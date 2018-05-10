4e3bd91eabe50c50bff9374d0072b894f0b9f80c
HOME <olga81koppel@gmail.com>
2018-05-10 22:39:23 +0300

    Utils (3 methods)

---
+++ src/main/java/Framework/Utils.java
@@ -0,0 +1,23 @@
+package Framework;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Utils extends BrowserManager {
+
+    public static void waitForElementVisible(WebElement someelement) {
+        new WebDriverWait(BrowserManager.browser, 3).until(ExpectedConditions.visibilityOf(someelement));
+    }
+
+    public static void navigateToDropDownList(WebElement menuElement) {
+        Actions action = new Actions(BrowserManager.browser);
+        action.moveToElement(menuElement).build().perform();
+    }
+
+    public static void clickWithJS(WebElement element) {
+        ((JavascriptExecutor) BrowserManager.browser).executeScript("arguments[0].click();", element);
+    }
+}
